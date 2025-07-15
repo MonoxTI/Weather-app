a566363492bc5d6e5260adc6ba41db6c9eed71a7
MonoxTI <itumonokoane84@gmail.com>
2025-07-15 17:32:28 +0200

    js

---
+++ backend.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('.weather');
+const input = document.querySelector('.city');
+const card = document.querySelector(".card");
+const api = f4700671a9959f33b41b29176917d7de;
+
+form.addEventListener("subumit", e =>{
+    
+});
+
+async function getweather(city){
+    const 
+}
+function displayweather(data)
+{
+
+}
+
+function getemoji(weatherid)
+{
+
+}
+
+function displayerror(message)
+{
+    
+}
